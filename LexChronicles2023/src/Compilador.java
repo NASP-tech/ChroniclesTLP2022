9f77dab8bad5fc8cbb509135687e9ac7a1c5d07d
Natalia Solorzano <00120319@uca.edu.sv>
2023-02-03 09:56:11 -0600

    :sparkles: Add: Form Design Compiler

--- LexChronicles2023/src/Compilador.java
+++ LexChronicles2023/src/Compilador.java
@@ -350,61 +350,7 @@ public class Compilador extends javax.swing.JFrame {
     }//GEN-LAST:event_btnEjecutarActionPerformed
 
     
-    private void executeCode(ArrayList<String> blocksOfCode, int repeats){
-        for(int j=1; j<=repeats; j++){
-            int repeatCode = -1;
-            for(int i=0; i<blocksOfCode.size(); i++){
-                String blockOfCode = blocksOfCode.get(i);
-                if(repeatCode!=-1){
-                    int[] posicionMarcador = CodeBlock.getPositionOfBothMarkers(blocksOfCode, blockOfCode);
-                    executeCode(new ArrayList<>(blocksOfCode.subList(posicionMarcador[0],posicionMarcador[1])), repeatCode);
-                    repeatCode = -1;
-                    i = posicionMarcador[1];
-                }
-                else{
-                    String[] sentences = blockOfCode.split(";");
-                    for(String sentence : sentences) {
-                        sentence = sentence.trim();
-                        if(sentence.startsWith("pintar")){
-                            String parametro;
-                            if(sentence.contains("$")){
-                                parametro = identificadores.get(sentence.substring(9, sentence.length()-2));
-                            }
-                            else{
-                                parametro = sentence.substring(9, sentence.length() -2);
-                            }
-                            System.out.println("Pintando de color " + parametro + "...");
-                        }
-                        else if(sentence.startsWith("izquierda")){
-                            System.out.println("Moviéndose a la izquierda...");
-                        }
-                        else if(sentence.startsWith("derecha")){
-                            System.out.println("Moviéndose a la derecha...");
-                        }
-                        else if(sentence.startsWith("adelante")){
-                            System.out.println("Moviéndose a la adelante...");
-                        }
-                        else if(sentence.startsWith("atrás")){
-                            System.out.println("Moviéndose a la atrás...");
-                        }
-                        else if(sentence.startsWith("-->")){
-                            System.out.println("Declarando identificador...");
-                        }
-                        else if(sentence.startsWith("repetir")){
-                            String parametro;
-                            if(sentence.contains("$")){
-                                parametro = identificadores.get(sentence.substring(10, sentence.length() -2));
-                            }
-                            else{
-                                parametro = sentence.substring(10, sentence.length()-2);
-                            }
-                            repeatCode = Integer.parseInt(parametro);
-                        }
-                    }
-                }
-            }
-        }
-    }
+    
     
     /*
         Llama los metodos de:
@@ -430,206 +376,22 @@ public class Compilador extends javax.swing.JFrame {
         Generar un archivo de entrada y uno de salida
         Se crea un lector del Buffer de los archivos
     */
-    private void lexicalAnalysis() {
-        // Extraer tokens
-        Lexer lexer;
-        try {
-            File codigo = new File("code.encrypter");
-            FileOutputStream output = new FileOutputStream(codigo);
-            byte[] bytesText = jtpCode.getText().getBytes();
-            output.write(bytesText);
-            BufferedReader entrada = new BufferedReader(new InputStreamReader(new FileInputStream(codigo), "UTF8"));
-            lexer = new Lexer(entrada);
-            while (true) {
-                Token token = lexer.yylex();
-                if (token == null) {
-                    break;
-                }
-                tokens.add(token);
-            }
-        } catch (FileNotFoundException ex) {
-            System.out.println("El archivo no pudo ser encontrado... " + ex.getMessage());
-        } catch (IOException ex) {
-            System.out.println("Error al escribir en el archivo... " + ex.getMessage());
-        }
-    }
+    
 
     /*
         Pasa a la gramática el arreglo de tokens
         Y muestra las gramáticas
     */
-    private void syntacticAnalysis() {
-        Grammar gramatica = new Grammar(tokens, errors);
+    
 
-        /*Eliminación de Errores*/
-        gramatica.delete(new String[] {"ERROR", "ERROR_1", "ERROR_2"},1);
-        
-        /*Agrupación de Valores*/
-        gramatica.group("VALOR", "(NUMERO | COLOR)", true);
-        
-        /*Declaración de Variables*/
-        gramatica.group("VARIABLE","TIPO_DATO IDENTIFICADOR OP_ASIG VALOR", true, identProd);        
-        gramatica.group("VARIABLE","TIPO_DATO OP_ASIG VALOR", true,
-                2, "error sintáctico {}: falta el identificador en la variable [#, %]");
-        
-        gramatica.finalLineColumn();
-        
-        gramatica.group("VARIABLE", "TIPO_DATO IDENTIFICADOR OP_ASIG", 3, 
-                "error sintáctico {}: falta el valor en la declaración [#, %]");
-        
-        gramatica.initialLineColumn();
-        
-        /*Eliminación de tipos de DAO y operadores de asignación*/
-        gramatica.delete("TIPO_DATO", 4,
-                "Error sintáctico {}: el tipo de dato no está en una declaración [#, %]");
-        
-        gramatica.delete("OP_ASIG", 5,
-                "Error sintáctico {}: el operador de asignación no está en una declaración [#, %]");
-        
-        /*Agrupación de identificadores y definición de parámetros*/
-        gramatica.group("VALOR", "IDENTIFICADOR", true);
-        gramatica.group("PARAMETROS", "VALOR (COMA VALOR)+");
-        
-        /*Agrupación de Funciones*/
-        gramatica.group("FUNCION", "(MOVIMIENTO | PINTAR | DETENER_PINTAR | TOMAR |"+
-                "LANZAR_MONEDA | VER | DETENER_REPETIR)", true);
-        gramatica.group("FUNCION_COMP", "FUNCION PARENTESIS_A (VALOR | PARAMETROS)? PARENTESIS_C", true);
-        gramatica.group("FUNCION_COMP", "FUNCION (VALOR | PARAMETROS)? PARENTESIS_C", true,
-                6, "error sintáctico {}: falta el paréntesis que abre en la función [#, %]");
-        gramatica.finalLineColumn();
-        
-        gramatica.group("FUNCION_COMP", "FUNCION PARENTESIS_A (VALOR | PARAMETROS)", true,
-                7, "error sintáctico {}: falta el paréntesis que abre en la función [#, %]");
-        
-        gramatica.initialLineColumn();
-        
-        /*Eliminación de funciones incompletas*/
-        gramatica.delete("FUNCION", 8, "error sintáctico {}: la función no está declarada correctamente");
-        
-        gramatica.loopForFunExecUntilChangeNotDetected(() -> {
-            gramatica.group("EXP_LOGICA", "(FUNCION_COMP | EXP_LOGICA) (OP_LOGICO (FUNCION_COMP | EXP_LOGICA))+");
-            gramatica.group("EXP_LOGICA", "PARENTESIS_A (EXP_LOGICA | FUNCION_COMP) PARENTESIS_C");
-        });   
-        
-        /*Eliminación de Operadores Logicos*/
-        gramatica.delete("OP_LOGICO", 10,
-                "error sintáctico {}: el operador logico no está contenido en una expresión");
-        
-        /*Agrupación de exp. logicas como valor y parametros*/
-        gramatica.group("VALOR", "EXP_LOGICA");
-        gramatica.group("PARAMETROS", "VALOR (COMA VALOR)+");
-        
-        /*Agrupacion de Estructuras de Control*/
-        gramatica.group("EST_CONTROL", "(REPETIR | ESTRUCTURA_SI)");
-        gramatica.group("EST_CONTROL_COMP", "EST_CONTROL PARENTESIS_A PARENTESIS_C");
-        gramatica.group("EST_CONTROL_COMP","EST_CONTROL (VALOR | PARAMETROS)");
-        gramatica.group("EST_CONTROL_COMP", "EST_CONTROL PARENTESIS_A (VALOR | PARAMETROS) PARENTESIS_C");
-        
-        /*Eliminación de Estructuras de Control Incompletas*/
-        gramatica.delete("EST_CONTROL", 11, 
-                "error sintáctico {}: la estructura de control no está declarada correctamente [#, %]");
-        
-        /*Eliminación de Parentesis*/
-        gramatica.delete(new String[]{"PARENTESIS_A", "PARENTESIS_C"}, 12, 
-        "error sintáctico {}: el paréntesis [] no está contenido en una agrupación [#, %]");
-        
-        gramatica.finalLineColumn();
-        
-        /*Verificación de punto y coma al final de una sentencia*/
-        //Identificadores o variables
-        gramatica.group("VARIABLE_PC", "VARIABLE PUNTO_COMA", true);
-        gramatica.group("VARIABLE_PC", "VARIABLE", true,
-                13, "error sintáctico {}: falta el punto y coma al final de la variable [#, %]");
-        
-        
-        /*Funciones*/
-        gramatica.group("FUNCION_COMP_PC", "FUNCION_COMP PUNTO_COMA");
-        gramatica.group("FUNCION_COMP_PC", "FUNCION_COMP", 14, 
-                "error sintáctico {}: falta el punto y coma al final de la declaración de la función");
-        
-        gramatica.initialLineColumn();
-        
-        /*Eliminación de punto y coma*/
-        gramatica.delete("PUNTO_COMA", 15,
-                "error sintáctico {}: el punto y coma no está al final de una sentencia");
-        
-        /*Sentencias*/
-        gramatica.group("SENTENCIAS", "(VARIABLE_PC | FUNCION_COMP_PC)+");
-        
-        gramatica.loopForFunExecUntilChangeNotDetected(() -> {
-            gramatica.group("EST_CONTROL_COMP_LASLC", "EST_CONTROL_COMP LLAVE_A (SENTENCIAS)? LLAVE_C", true);
-            gramatica.group("SENTENCIAS", "(SENTENCIAS | EST_CONTROL_COMP_LASLC)+");
-        });
-                
-        /*Estructuras de función incompletas*/
-        gramatica.loopForFunExecUntilChangeNotDetected(() -> {
-            gramatica.initialLineColumn();
-            gramatica.group("EST_CONTROL_COMP_LASLC", "EST_CONTROL_COMP (SENTENCIAS)? LLAVE_C", true, 
-                    16, "error sintáctico {}: falta la llave que abre en la estructura de control");
-            
-            gramatica.finalLineColumn();
-            
-            gramatica.group("EST_CONTROL_COMP_LASLC", "EST_CONTROL_COMP LLAVE_A SENTENCIAS", true,
-                    17, "error sintáctico {}: falta la llave que cierra en la estructura de control");
-            
-            gramatica.group("SENTENCIAS", "(SENTENCIAS | EST_CONTROL_COMP_LASLC)");
-        });
-        
-        gramatica.delete(new String[] {"LLAVE_A", "LLAVE_C"}, 18,
-                "error sintáctico {}: la llave [] no está contenida en una agrupación [#, %]");
-                
-        /* Mostrar gramáticas */
-        gramatica.show();
-    }
 
-    private void semanticAnalysis() {
-        HashMap<String, String> identDataType = new HashMap<>();
-        identDataType.put("color", "COLOR");
-        identDataType.put("número", "NUMERO");
-        for(Production id: identProd) {
-            if(!identDataType.get(id.lexemeRank(0)).equals(id.lexicalCompRank(-1))){
-                errors.add(new ErrorLSSL(1, "error semántico {}: valor no compatible con el tipo de dato [#, %]", id, true));
-            }
-            else if(id.lexicalCompRank(-1).equals("COLOR") && !id.lexemeRank(-1).matches("#[0-9a-fA-F]+")){
-                errors.add(new ErrorLSSL(2, "error semántico {}: el color no es un número hexadecimal [#, %]", id, false));
-            }
-            else {
-                identificadores.put(id.lexemeRank(1), id.lexemeRank(-1));
-            }
-        }
-    }
 
     /*
         Limpia el arreglo de los textos de color
         Retorna un color lexico
         Llama al metodo, con el color por defecto
     */
-    private void colorAnalysis() {
-        /* Limpiar el arreglo de colores */
-        textsColor.clear();
-        /* Extraer rangos de colores */
-        LexerColor lexerColor;
-        try {
-            File codigo = new File("color.encrypter");
-            FileOutputStream output = new FileOutputStream(codigo);
-            byte[] bytesText = jtpCode.getText().getBytes();
-            output.write(bytesText);
-            BufferedReader entrada = new BufferedReader(new InputStreamReader(new FileInputStream(codigo), "UTF8"));
-            lexerColor = new LexerColor(entrada);
-            while (true) {
-                TextColor textColor = lexerColor.yylex();
-                if (textColor == null) {
-                    break;
-                }
-                textsColor.add(textColor);
-            }
-        } catch (FileNotFoundException ex) {
-            System.out.println("El archivo no pudo ser encontrado... " + ex.getMessage());
-        } catch (IOException ex) {
-            System.out.println("Error al escribir en el archivo... " + ex.getMessage());
-        }
-        Functions.colorTextPane(textsColor, jtpCode, new Color(40, 40, 40));
-    }
+
 
     /*
         Recorre todos los tokens
